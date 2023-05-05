84989f8a492f43e870f7f07f2bcc9601b88f1c75
GitHub Action <16268065+xnkjj@users.noreply.github.com>
2023-05-05 13:07:31 +0000

    feat: updated by action, Fri May  5 13:07:31 UTC 2023

--- clients/doc.go
+++ clients/doc.go
@@ -34,7 +34,7 @@
 //	// - It may require correct/in-range values for request initialization.
 //	// - It may require specifying regional endpoints when creating the service client as shown in:
 //	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
-//	c, err := clients.NewFeedClient(ctx)
+//	c, err := clients.NewAdGroupAdLabelClient(ctx)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
@@ -54,17 +54,17 @@
 //	// - It may require correct/in-range values for request initialization.
 //	// - It may require specifying regional endpoints when creating the service client as shown in:
 //	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
-//	c, err := clients.NewFeedClient(ctx)
+//	c, err := clients.NewAdGroupAdLabelClient(ctx)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
 //	defer c.Close()
 //
-//	req := &servicespb.MutateFeedsRequest{
+//	req := &servicespb.MutateAdGroupAdLabelsRequest{
 //		// TODO: Fill request struct fields.
-//		// See https://pkg.go.dev/github.com/shenzhencenter/google-ads-pb/services#MutateFeedsRequest.
+//		// See https://pkg.go.dev/github.com/shenzhencenter/google-ads-pb/services#MutateAdGroupAdLabelsRequest.
 //	}
-//	resp, err := c.MutateFeeds(ctx, req)
+//	resp, err := c.MutateAdGroupAdLabels(ctx, req)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
@@ -73,7 +73,7 @@
 //
 // # Use of Context
 //
-// The ctx passed to NewFeedClient is used for authentication requests and
+// The ctx passed to NewAdGroupAdLabelClient is used for authentication requests and
 // for creating the underlying connection, but is not used for subsequent calls.
 // Individual methods on the client use the ctx given to them.
 //
--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/shenzhencenter/google-ads-pb
 go 1.14
 
 require (
-	cloud.google.com/go v0.97.0
-	github.com/googleapis/gax-go/v2 v2.1.1
-	google.golang.org/api v0.60.0
-	google.golang.org/genproto v0.0.0-20211112145013-271947fe86fd
-	google.golang.org/grpc v1.50.1
-	google.golang.org/protobuf v1.27.1
+	cloud.google.com/go/longrunning v0.4.1
+	github.com/googleapis/gax-go/v2 v2.7.0
+	google.golang.org/api v0.110.0
+	google.golang.org/genproto v0.0.0-20230209215440-0dfe4f8abfcc
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.28.1
 )
