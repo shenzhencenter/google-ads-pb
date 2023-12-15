cde97c0da7bf70fe44474582fb71a20522300836
GitHub Action <16268065+xnkjj@users.noreply.github.com>
2023-12-15 19:12:43 +0000

    feat: updated by action, Fri Dec 15 19:12:43 UTC 2023

--- clients/batch_job_client.go
+++ clients/batch_job_client.go
@@ -27,7 +27,6 @@ import (
 	lroauto "cloud.google.com/go/longrunning/autogen"
 	longrunningpb "cloud.google.com/go/longrunning/autogen/longrunningpb"
 	gax "github.com/googleapis/gax-go/v2"
-	resourcespb "github.com/shenzhencenter/google-ads-pb/resources"
 	servicespb "github.com/shenzhencenter/google-ads-pb/services"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
@@ -427,107 +426,10 @@ func (c *batchJobGRPCClient) AddBatchJobOperations(ctx context.Context, req *ser
 	return resp, nil
 }
 
-// RunBatchJobOperation manages a long-running operation from RunBatchJob.
-type RunBatchJobOperation struct {
-	lro *longrunning.Operation
-}
-
 // RunBatchJobOperation returns a new RunBatchJobOperation from a given name.
 // The name must be that of a previously created RunBatchJobOperation, possibly from a different process.
 func (c *batchJobGRPCClient) RunBatchJobOperation(name string) *RunBatchJobOperation {
 	return &RunBatchJobOperation{
 		lro: longrunning.InternalNewOperation(*c.LROClient, &longrunningpb.Operation{Name: name}),
 	}
-}
-
-// Wait blocks until the long-running operation is completed, returning the response and any errors encountered.
-//
-// See documentation of Poll for error-handling information.
-func (op *RunBatchJobOperation) Wait(ctx context.Context, opts ...gax.CallOption) error {
-	return op.lro.WaitWithInterval(ctx, nil, time.Minute, opts...)
-}
-
-// Poll fetches the latest state of the long-running operation.
-//
-// Poll also fetches the latest metadata, which can be retrieved by Metadata.
-//
-// If Poll fails, the error is returned and op is unmodified. If Poll succeeds and
-// the operation has completed with failure, the error is returned and op.Done will return true.
-// If Poll succeeds and the operation has completed successfully,
-// op.Done will return true, and the response of the operation is returned.
-// If Poll succeeds and the operation has not completed, the returned response and error are both nil.
-func (op *RunBatchJobOperation) Poll(ctx context.Context, opts ...gax.CallOption) error {
-	return op.lro.Poll(ctx, nil, opts...)
-}
-
-// Metadata returns metadata associated with the long-running operation.
-// Metadata itself does not contact the server, but Poll does.
-// To get the latest metadata, call this method after a successful call to Poll.
-// If the metadata is not available, the returned metadata and error are both nil.
-func (op *RunBatchJobOperation) Metadata() (*resourcespb.BatchJob_BatchJobMetadata, error) {
-	var meta resourcespb.BatchJob_BatchJobMetadata
-	if err := op.lro.Metadata(&meta); err == longrunning.ErrNoMetadata {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return &meta, nil
-}
-
-// Done reports whether the long-running operation has completed.
-func (op *RunBatchJobOperation) Done() bool {
-	return op.lro.Done()
-}
-
-// Name returns the name of the long-running operation.
-// The name is assigned by the server and is unique within the service from which the operation is created.
-func (op *RunBatchJobOperation) Name() string {
-	return op.lro.Name()
-}
-
-// BatchJobResultIterator manages a stream of *servicespb.BatchJobResult.
-type BatchJobResultIterator struct {
-	items    []*servicespb.BatchJobResult
-	pageInfo *iterator.PageInfo
-	nextFunc func() error
-
-	// Response is the raw response for the current page.
-	// It must be cast to the RPC response type.
-	// Calling Next() or InternalFetch() updates this value.
-	Response interface{}
-
-	// InternalFetch is for use by the Google Cloud Libraries only.
-	// It is not part of the stable interface of this package.
-	//
-	// InternalFetch returns results from a single call to the underlying RPC.
-	// The number of results is no greater than pageSize.
-	// If there are no more results, nextPageToken is empty and err is nil.
-	InternalFetch func(pageSize int, pageToken string) (results []*servicespb.BatchJobResult, nextPageToken string, err error)
-}
-
-// PageInfo supports pagination. See the google.golang.org/api/iterator package for details.
-func (it *BatchJobResultIterator) PageInfo() *iterator.PageInfo {
-	return it.pageInfo
-}
-
-// Next returns the next result. Its second return value is iterator.Done if there are no more
-// results. Once Next returns Done, all subsequent calls will return Done.
-func (it *BatchJobResultIterator) Next() (*servicespb.BatchJobResult, error) {
-	var item *servicespb.BatchJobResult
-	if err := it.nextFunc(); err != nil {
-		return item, err
-	}
-	item = it.items[0]
-	it.items = it.items[1:]
-	return item, nil
-}
-
-func (it *BatchJobResultIterator) bufLen() int {
-	return len(it.items)
-}
-
-func (it *BatchJobResultIterator) takeBuf() interface{} {
-	b := it.items
-	it.items = nil
-	return b
 }
--- clients/doc.go
+++ clients/doc.go
@@ -41,7 +41,7 @@
 //	// - It may require correct/in-range values for request initialization.
 //	// - It may require specifying regional endpoints when creating the service client as shown in:
 //	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
-//	c, err := clients.NewAdGroupCriterionLabelClient(ctx)
+//	c, err := clients.NewAdGroupBidModifierClient(ctx)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
@@ -61,17 +61,17 @@
 //	// - It may require correct/in-range values for request initialization.
 //	// - It may require specifying regional endpoints when creating the service client as shown in:
 //	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
-//	c, err := clients.NewAdGroupCriterionLabelClient(ctx)
+//	c, err := clients.NewAdGroupBidModifierClient(ctx)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
 //	defer c.Close()
 //
-//	req := &servicespb.MutateAdGroupCriterionLabelsRequest{
+//	req := &servicespb.MutateAdGroupBidModifiersRequest{
 //		// TODO: Fill request struct fields.
-//		// See https://pkg.go.dev/github.com/shenzhencenter/google-ads-pb/services#MutateAdGroupCriterionLabelsRequest.
+//		// See https://pkg.go.dev/github.com/shenzhencenter/google-ads-pb/services#MutateAdGroupBidModifiersRequest.
 //	}
-//	resp, err := c.MutateAdGroupCriterionLabels(ctx, req)
+//	resp, err := c.MutateAdGroupBidModifiers(ctx, req)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
@@ -80,7 +80,7 @@
 //
 // # Use of Context
 //
-// The ctx passed to NewAdGroupCriterionLabelClient is used for authentication requests and
+// The ctx passed to NewAdGroupBidModifierClient is used for authentication requests and
 // for creating the underlying connection, but is not used for subsequent calls.
 // Individual methods on the client use the ctx given to them.
 //
