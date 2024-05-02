7a74e681515834400f204b59caae04616d54639c
GitHub Action <16268065+xnkjj@users.noreply.github.com>
2024-05-02 17:29:48 +0000

    feat: updated by action, Thu May  2 17:29:48 UTC 2024

--- clients/doc.go
+++ clients/doc.go
@@ -41,7 +41,7 @@
 //	// - It may require correct/in-range values for request initialization.
 //	// - It may require specifying regional endpoints when creating the service client as shown in:
 //	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
-//	c, err := clients.NewCustomerSkAdNetworkConversionValueSchemaClient(ctx)
+//	c, err := clients.NewCustomerConversionGoalClient(ctx)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
@@ -61,17 +61,17 @@
 //	// - It may require correct/in-range values for request initialization.
 //	// - It may require specifying regional endpoints when creating the service client as shown in:
 //	//   https://pkg.go.dev/cloud.google.com/go#hdr-Client_Options
-//	c, err := clients.NewCustomerSkAdNetworkConversionValueSchemaClient(ctx)
+//	c, err := clients.NewCustomerConversionGoalClient(ctx)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
 //	defer c.Close()
 //
-//	req := &servicespb.MutateCustomerSkAdNetworkConversionValueSchemaRequest{
+//	req := &servicespb.MutateCustomerConversionGoalsRequest{
 //		// TODO: Fill request struct fields.
-//		// See https://pkg.go.dev/github.com/shenzhencenter/google-ads-pb/services#MutateCustomerSkAdNetworkConversionValueSchemaRequest.
+//		// See https://pkg.go.dev/github.com/shenzhencenter/google-ads-pb/services#MutateCustomerConversionGoalsRequest.
 //	}
-//	resp, err := c.MutateCustomerSkAdNetworkConversionValueSchema(ctx, req)
+//	resp, err := c.MutateCustomerConversionGoals(ctx, req)
 //	if err != nil {
 //		// TODO: Handle error.
 //	}
@@ -80,7 +80,7 @@
 //
 // # Use of Context
 //
-// The ctx passed to NewCustomerSkAdNetworkConversionValueSchemaClient is used for authentication requests and
+// The ctx passed to NewCustomerConversionGoalClient is used for authentication requests and
 // for creating the underlying connection, but is not used for subsequent calls.
 // Individual methods on the client use the ctx given to them.
 //
